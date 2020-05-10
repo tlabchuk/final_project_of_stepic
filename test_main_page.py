f4d79265020606de8d6f7605010129b8594e70ad
tanja.labchuk <tanja.labchuk@gmail.com>
2020-05-10 12:03:27 +0300

    Fix code

--- test_main_page.py
+++ test_main_page.py
@@ -14,8 +14,7 @@ class TestLoginFromMainPage():
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-
-    def test_guest_should_see_login_link(self,browser):
+    def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
