7369bcef141220d0f6f86e0882919d235c96739f
tanja.labchuk <tanja.labchuk@gmail.com>
2020-05-10 11:47:45 +0300

    Update file to fix error

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,25 @@ from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 import pytest
-import time
+
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+         pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+         "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.check_name_of_book()
+    page.check_price_of_book()
 
 @pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -12,6 +30,11 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.should_not_be_success_message()
 
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
 
 @pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
@@ -70,4 +93,3 @@ class TestUserAddToBasketFromProductPage:
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
-
